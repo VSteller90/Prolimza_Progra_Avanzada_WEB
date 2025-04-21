24bb97e99106a8a8f2c3344726d0b532a7403042
VSteller1990 <vse10000@hotmail.com>
2025-04-21 17:16:21 -0600

    Adding Layout

--- Prolimza/Prolimza/Controllers/DetalleVentasController.cs
+++ Prolimza/Prolimza/Controllers/DetalleVentasController.cs
@@ -18,11 +18,7 @@ namespace Prolimza.Controllers
             _context = context;
         }
 
-        // GET: DetalleVentas
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.DetallesVenta.ToListAsync());
-        }
+
 
         // GET: DetalleVentas/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -64,19 +60,21 @@ namespace Prolimza.Controllers
             return View(detalleVenta);
         }
 
-        // GET: DetalleVentas/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        // GET: DetalleVentas/Edit/5/10
+        public async Task<IActionResult> Edit(int? idVenta, int? idProducto)
         {
-            if (id == null)
+            if (idVenta == null || idProducto == null)
             {
                 return NotFound();
             }
 
-            var detalleVenta = await _context.DetallesVenta.FindAsync(id);
+            var detalleVenta = await _context.DetallesVenta
+                .FirstOrDefaultAsync(m => m.IdVenta == idVenta && m.IdProducto == idProducto);
             if (detalleVenta == null)
             {
                 return NotFound();
             }
+
             return View(detalleVenta);
         }
 
@@ -116,15 +114,15 @@ namespace Prolimza.Controllers
         }
 
         // GET: DetalleVentas/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? idVenta, int? idProducto)
         {
-            if (id == null)
+            if (idVenta == null || idProducto == null)
             {
                 return NotFound();
             }
 
             var detalleVenta = await _context.DetallesVenta
-                .FirstOrDefaultAsync(m => m.IdVenta == id);
+                .FirstOrDefaultAsync(m => m.IdVenta == idVenta && m.IdProducto == idProducto);
             if (detalleVenta == null)
             {
                 return NotFound();
@@ -133,21 +131,59 @@ namespace Prolimza.Controllers
             return View(detalleVenta);
         }
 
-        // POST: DetalleVentas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int idVenta, int idProducto)
         {
-            var detalleVenta = await _context.DetallesVenta.FindAsync(id);
+            var detalleVenta = await _context.DetallesVenta
+                .FirstOrDefaultAsync(m => m.IdVenta == idVenta && m.IdProducto == idProducto);
+
             if (detalleVenta != null)
             {
                 _context.DetallesVenta.Remove(detalleVenta);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
+        // GET: DetalleVenta/CreateMultiple
+        public IActionResult CreateMultiple(int idVenta)
+        {
+            var productos = _context.Productos.ToList();
+            ViewBag.IdVenta = idVenta;
+            return View(productos);
+        }
+
+        // POST: DetalleVenta/CreateMultiple
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateMultiple(int idVenta, List<DetalleVenta> detalles)
+        {
+            foreach (var detalle in detalles)
+            {
+                if (detalle.Cantidad > 0)
+                {
+                    detalle.IdVenta = idVenta;
+                    _context.DetallesVenta.Add(detalle);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            return View();
+        }
+
+
+        // GET: Ventas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ventas.ToListAsync());
+        }
+
+        public async Task<IActionResult> ListaAsync()
+        {
+            return View(await _context.DetallesVenta.ToListAsync());
+        }
         private bool DetalleVentaExists(int id)
         {
             return _context.DetallesVenta.Any(e => e.IdVenta == id);
